eacb0293840f55010cda33e0bc84dfd119355b02
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-03-29 22:37:36 +0000

    Updates

---
+++ precache-manifest.4b8405cf84ab2816a40bb027b571588f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ae699ea3cc63fef9c8",
+    "url": "/weatherApp/static/js/runtime~main.25533534.js"
+  },
+  {
+    "revision": "efda5e539b89206b0a25",
+    "url": "/weatherApp/static/js/main.5f10c685.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/weatherApp/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "efda5e539b89206b0a25",
+    "url": "/weatherApp/static/css/main.38d9317a.chunk.css"
+  },
+  {
+    "revision": "ccc817aa3815ba1311264dc1d7f21bc0",
+    "url": "/weatherApp/index.html"
+  }
+];
